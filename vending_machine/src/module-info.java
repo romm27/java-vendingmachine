533ca30e42abe5475a381bf83afd9d65b39ef3f9
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-10-21 14:28:06 -0300

    Initial commit

---
+++ vending_machine/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module vending_machine {
+}
---
+++ vending_machine/src/vending_machine/Main.java
@@ -0,0 +1,10 @@
+package vending_machine;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
