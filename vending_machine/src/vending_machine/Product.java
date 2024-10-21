d0c63390881609de098b064b92547dc5743894bb
Ana Borowsky <114084376+ana-borowsky@users.noreply.github.com>
2024-10-21 16:17:19 -0300

    Implements Product

--- vending_machine/src/vending_machine/Product.java
+++ vending_machine/src/vending_machine/Product.java
@@ -1,5 +1,26 @@
 package vending_machine;
 
 public class Product {
-
+	private String name;
+	private String brand;
+	private int price;
+	private int stock;
+	
+    public Product(String name, String brand, int price, int stock) {
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.stock = stock;
+    }
+    
+    public boolean sell() {
+    	// Add later check if there is enough cash
+    	if (this.stock > 0) {
+    		this.stock -= 1;
+    		
+    		return true;
+    	} else { return false; }
+    }
+    
+    
 }
