c85a857753448029d43ca937be358daed40d2230
Gusta Dev <gustacamara.dev@hotmail.com>
2024-10-22 08:32:30 -0300

    Registry print history

---
+++ vending_machine/src/vending_machine/Sale.java
@@ -0,0 +1,19 @@
+package vending_machine;
+
+public class Sale {
+    private String name;
+
+    public Sale(String name) {
+    	this.name = name;
+    }
+    
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+    
+}
