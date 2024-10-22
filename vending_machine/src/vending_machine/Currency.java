62b55ec7d412b04240d5347fcf1852eeef62db60
Giovanni Strasser <giostrasser@gmail.com>
2024-10-22 01:20:30 -0300

    Implements money printing

--- vending_machine/src/vending_machine/Currency.java
+++ vending_machine/src/vending_machine/Currency.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Currency {
 	public enum CurrencyType{ bill, coin}
-	private int quantityAvailable;
+	private int quantityInStock;
 	private String name;
 	private int value;
 	private int divider;
@@ -13,7 +13,7 @@ public class Currency {
 	
 	// Defines the available bills
 	static {
-		availableBills.add(new Currency(10, "Um Centavo", 5, 100, CurrencyType.coin));
+		availableBills.add(new Currency(10, "Um Centavo", 1, 100, CurrencyType.coin));
 		availableBills.add(new Currency(10, "Cinco Centavos", 5, 100, CurrencyType.coin));
 		availableBills.add(new Currency(10, "Dez Centavos", 10, 100, CurrencyType.coin));
 		availableBills.add(new Currency(10, "Vinte e Cinco Centavos", 25, 100, CurrencyType.coin));
@@ -24,17 +24,44 @@ public class Currency {
 		availableBills.add(new Currency(10, "Dez Reais", 10, 1, CurrencyType.bill));
 		availableBills.add(new Currency(10, "Cinquenta Reais", 50, 1, CurrencyType.bill));
 		availableBills.add(new Currency(10, "Cem Reais", 100, 1, CurrencyType.bill));
+		
 
 	}
 	
 	public static void main(String[] args) {
-		for(int i = 0; i < availableBills.size();i++) {
-			System.out.println("R$" + availableBills.get(i).getValue());
-		}
-
+		printCurrency(availableBills);
 	}
 	
-	public Currency(int quantityAvailable, String name, int value, int divider, CurrencyType currencyType) {
+	public static ArrayList<Currency> getExchange(float value){
+		ArrayList<Currency> temp = new ArrayList<Currency>();
+		
+		for(int i = availableBills.size(); i > 0; i--) {
+			for(int j = 0; j < availableBills.get(i - 1).quantityInStock; j++) {
+				if(value > availableBills.get(j).value) {
+					value -= availableBills.get(j).value;
+					temp.add(availableBills.get(j));
+				}
+			}
+		}
+		
+		return temp;
+	}
+	
+	public static void printCurrency(ArrayList<Currency> currencyList) {
+		int acumulated = 1;
+		for(int i = 0; i < currencyList.size(); i++) {
+			Currency current = currencyList.get(i);
+			Currency next = (i == (currencyList.size() - 1))? null : currencyList.get(i + 1);
+			if(next == null || current.getDoubleValue() != next.getDoubleValue()) {
+				System.out.printf("%dx - %s" + System.lineSeparator(), acumulated, currencyList.get(i).name);
+				acumulated = 0;
+			}
+			acumulated += 1;
+		}
+	}
+	
+	public Currency(int quantityInStock, String name, int value, int divider, CurrencyType currencyType) {
+		this.quantityInStock = quantityInStock;
 		this.name = name;
 		this.value = value;
 		this.divider = divider;
@@ -53,7 +80,7 @@ public class Currency {
 	public void setValue(int value) {
 		this.value = value;
 	}
-	public double getValue() {
+	public double getDoubleValue() {
 		return (double)value/divider;
 	}
 
@@ -62,10 +89,10 @@ public class Currency {
 	}
 
 	public int getQuantityAvailable() {
-		return quantityAvailable;
+		return quantityInStock;
 	}
 
 	public void setQuantityAvailable(int quantityAvailable) {
-		this.quantityAvailable = quantityAvailable;
+		this.quantityInStock = quantityAvailable;
 	}
 }
