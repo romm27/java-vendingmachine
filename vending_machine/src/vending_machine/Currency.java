44824f03e4feeadc61233505902e6c8814fc24a3
Giovanni Strasser <giostrasser@gmail.com>
2024-10-21 22:21:32 -0300

    Convert currency from array to ArrayList

--- vending_machine/src/vending_machine/Currency.java
+++ vending_machine/src/vending_machine/Currency.java
@@ -1,31 +1,34 @@
 package vending_machine;
 
+import java.util.ArrayList;
+
 public class Currency {
 	public enum CurrencyType{ bill, coin}
 	private String name;
 	private int value;
 	private int divider;
 	private CurrencyType currencyType;
-	public static Currency[] availableBills = new Currency[10];
+	public static  ArrayList<Currency> availableBills = new ArrayList<Currency>();
 	
 	// Defines the available bills
-	// 5, 10, 25, 50
 	static {
-		availableBills[0] = new Currency("Cinco Centavos", 5, 100, CurrencyType.coin);
-		availableBills[1] = new Currency("Dez centavos", 10, 100, CurrencyType.coin);
-		availableBills[2] = new Currency("Vinte e cinco centavos", 25, 100, CurrencyType.coin);
-		availableBills[3] = new Currency("Cinquenta Centavos", 5, 10, CurrencyType.coin);
-		availableBills[4] = new Currency("Um Real", 1, 1, CurrencyType.coin);
-		availableBills[5] = new Currency("Dois Reais", 2, 1, CurrencyType.bill);
-		availableBills[6] = new Currency("Cinco Reais", 5, 1, CurrencyType.bill);
-		availableBills[7] = new Currency("Dez Reais", 10, 1, CurrencyType.bill);
-		availableBills[8] = new Currency("Cinquenta Reais", 50, 1, CurrencyType.bill);
-		availableBills[9] = new Currency("Cem Reais", 100, 1, CurrencyType.bill);
+		availableBills.add(new Currency("Um Centavo", 5, 100, CurrencyType.coin));
+		availableBills.add(new Currency("Cinco Centavos", 5, 100, CurrencyType.coin));
+		availableBills.add(new Currency("Dez Centavos", 10, 100, CurrencyType.coin));
+		availableBills.add(new Currency("Vinte e Cinco Centavos", 25, 100, CurrencyType.coin));
+		availableBills.add(new Currency("Cinquenta Centavos", 5, 10, CurrencyType.coin));
+		availableBills.add(new Currency("Um Real", 1, 1, CurrencyType.coin));
+		availableBills.add(new Currency("Dois Reais", 2, 1, CurrencyType.bill));
+		availableBills.add(new Currency("Cinco Reais", 5, 1, CurrencyType.bill));
+		availableBills.add(new Currency("Dez Reais", 10, 1, CurrencyType.bill));
+		availableBills.add(new Currency("Cinquenta Reais", 50, 1, CurrencyType.bill));
+		availableBills.add(new Currency("Cem Reais", 100, 1, CurrencyType.bill));
+
 	}
 	
 	public static void main(String[] args) {
-		for(int i = 0; i < availableBills.length;i++) {
-			System.out.println("R$" + availableBills[i].getValue());
+		for(int i = 0; i < availableBills.size();i++) {
+			System.out.println("R$" + availableBills.get(i).getValue());
 		}
 
 	}
