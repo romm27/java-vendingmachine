436892084ef75c7e42ab7fd76e084ec06d4eefe3
Gusta Dev <gustacamara.dev@hotmail.com>
2024-10-23 18:09:42 -0300

    print registry and registrysell

--- vending_machine/src/vending_machine/Sale.java
+++ vending_machine/src/vending_machine/Sale.java
@@ -1,28 +1,66 @@
 package vending_machine;
+
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+
+import vending_machine.Product.ProductType;
 //ArrayList sales, generate relatory of sales,
 
 public class Sale {
+	public static void main(String[] args) { 	
+        Product product = new Product("salgado", "do gusta", ProductType.food, 7_00, 18 );
+        Sale sale = new Sale(product);
+        sales.add(sale);
+        sales.add(sale);
+        sales.add(sale);
+        sales.add(sale);
+        sales.add(sale);
+        sales.add(sale);
+        sales.add(sale);
+        sales.add(sale);
+        sales.add(sale);
+        sale.printSaleHistory();
+        
+    }
+	
     private static ArrayList<Sale> sales = new ArrayList<>();
     private Product product;
+    private String time;
+    private String date;
     
- 
+    public Sale (Product product) {
+    	sellRegister(product);
+    }
+    
 	public void printSaleHistory(){
         System.out.println("Aqui está o histórico de vendas:");
         double totalValue = 0;
-        for(int i = 0; i < sales.size(); i++){
-            System.out.printf("O produto %s custou %f reais \n",
-            sales.get(i).product.getName(),
-            sales.get(i).product.getPrice()
+        for(Sale s: sales){
+            System.out.printf("O produto '%s' custou %.2fR$ | Data da venda: %s às %sh.\n",
+            s.product.getName(),
+            s.product.getPrice(),
+            s.getDate(),
+            s.getTime()
             );
-         totalValue = totalValue + sales.get(i).product.getPrice();
+         totalValue = totalValue + s.product.getPrice();
         }
-        System.out.printf("\nO valor total de vendas foi de: %.2f", totalValue);
+        System.out.printf("\nO valor total de vendas foi de: %.2fR$", totalValue);
 	}
 	
-	
-    public static void main(String[] args) { 	
-        
-    }
+	public void sellRegister(Product product) {
+		Format timeFormat = new SimpleDateFormat("HH:mm");
+		Format dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		time = timeFormat.format(new Date());
+		date = dateFormat.format(new Date());
+		this.product = product;
+	}
     
+    public String getTime() {
+    	return time;
+    }
+    public String getDate() {
+    	return date;
+    }
 }
