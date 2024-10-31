641dcf5be3ededcd0e8da514dfddd4b430daeb17
Gusta Dev <gustacamara.dev@hotmail.com>
2024-10-31 19:24:54 -0300

    implements invalid math input exeption

--- vending_machine/src/vending_machine/VendingMachineConsole.java
+++ vending_machine/src/vending_machine/VendingMachineConsole.java
@@ -1,4 +1,5 @@
 package vending_machine;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class VendingMachineConsole {
@@ -6,6 +7,11 @@ public class VendingMachineConsole {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         VendingMachine vendingMachine = new VendingMachine();
+
+        Product amendoim = new Product(0, "Amendoim", "Yoki", Product.ProductType.food, 200, 10);
+        Product salgadinho = new Product(1, "Salgadinho", "Elma Chips", Product.ProductType.food, 300, 10);
+        Product guarana = new Product(2, "Guaraná Zero", "Coca-Cola", Product.ProductType.beverage, 250, 10);
+        Product cafe = new Product(3, "Café", "3 Corações", Product.ProductType.beverage, 150, 10);
 
 
         boolean menu = true;
@@ -22,40 +28,43 @@ public class VendingMachineConsole {
             int option = scanner.nextInt();
 
             if (option >= 1 && option <= 4) {
-                System.out.println("Realize o pagamento: "); // Modify later
-                double payment = scanner.nextDouble();
-                
-                switch (option) {
-                    case 1:
-                        System.out.println("Você escolheu Amendoim.");
-                        // vendingMachine.sell(amendoim, payment);
-                        break;
-                    case 2:
-                        System.out.println("Você escolheu Salgadinho.");
-                        // vendingMachine.sell(salgadinho, payment);
-                        break;
-                    case 3:
-                        System.out.println("Você escolheu Guaraná Zero.");
-                        // vendingMachine.sell(guarana, payment);
-                        break;
-                    case 4:
-                        System.out.println("Você escolheu Café.");
-                        // vendingMachine.sell(cafe, payment);
-                        break;
-                    default:
-                        break;
+                try{
+                    System.out.println("Realize o pagamento: "); // Modify later
+                    double payment = scanner.nextDouble();
+                    switch (option) {
+                        case 1:
+                            System.out.println("Você escolheu Amendoim.");
+                            vendingMachine.sell(amendoim, payment);
+                            break;
+                        case 2:
+                            System.out.println("Você escolheu Salgadinho.");
+                            vendingMachine.sell(salgadinho, payment);
+                            break;
+                        case 3:
+                            System.out.println("Você escolheu Guaraná Zero.");
+                            vendingMachine.sell(guarana, payment);
+                            break;
+                        case 4:
+                            System.out.println("Você escolheu Café.");
+                            vendingMachine.sell(cafe, payment);
+                            break;
+                        default:
+                            break;
+                    }
+                }catch (InputMismatchException e) {
+                    System.out.println("Apenas numero são permitidos. Tente novamente.");
+                    scanner.next();
+                    continue;
                 }
             } else if (option == 5) {
             	
                 System.out.println("Obrigado por usar a máquina de vendas! Volte sempre!");
                 menu = false;
-                
             } else {
             	
                 System.out.println("Opção inválida. Tente novamente.");
             }
         }
-
         scanner.close();
     }
 }
