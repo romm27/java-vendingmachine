f5684b5e150fa4b5abd9f6ad0fe6b86ed5af34e1
Giovanni Strasser <giostrasser@gmail.com>
2024-10-31 11:03:57 -0300

    Fix image loading issue

--- vending_machine/src/vending_machine/ProgramGraphics.java
+++ vending_machine/src/vending_machine/ProgramGraphics.java
@@ -8,8 +8,10 @@ public abstract class ProgramGraphics {
 		JFrame w = new JFrame();
 		w.setBounds(130,100,100, 40);
 		w.setSize(16 *screenSizeMultiplier,9 * screenSizeMultiplier);
+		w.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		w.setVisible(true);
 		w.add(new JLabel(new ImageIcon("images/temp.jpeg")));
+		w.revalidate();
 		return w;
 	}
 	
