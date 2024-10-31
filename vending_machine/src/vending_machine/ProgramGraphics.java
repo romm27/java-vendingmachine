507ffe8398f3a2f48c28439268790040d9e00cdc
Giovanni Strasser <giostrasser@gmail.com>
2024-10-31 10:37:57 -0300

    Implement screen size configs

--- vending_machine/src/vending_machine/ProgramGraphics.java
+++ vending_machine/src/vending_machine/ProgramGraphics.java
@@ -2,10 +2,12 @@ package vending_machine;
 import javax.swing.*;
 
 public abstract class ProgramGraphics {
+	public static int screenSizeMultiplier = 120;
+	
 	public static JFrame CreateFrame() {
 		JFrame w = new JFrame();
 		w.setBounds(130,100,100, 40);
-		w.setSize(500,400);
+		w.setSize(16 *screenSizeMultiplier,9 * screenSizeMultiplier);
 		w.setVisible(true);
 		return w;
 	}
